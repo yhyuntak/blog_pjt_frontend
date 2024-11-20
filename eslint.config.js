45dce6732f718177fc2638faf4735b2350f59232
yhyuntak <yhyuntak@gmail.com>
2024-11-20 20:13:05 +0900

    [MODIFY] eslint.config.js 수정

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,26 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import {defineConfig} from "vite";
 
-export default defineConfig[
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,ts,jsx,tsx}"] },
+  {
+    languageOptions: {
+      globals: globals.browser, // window, console, alert 같은 browser 변수를 사용할 수 있도록 함.
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    // rules: {
+    //   semi: ["error", "always"],
+    // },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  {
-    rules: {
-      "react/react-in-jsx-scope": "off",
-    },
-  },
 ];
