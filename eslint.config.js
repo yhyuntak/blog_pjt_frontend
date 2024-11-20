c259706d8b65ff2d0174c9e87d7788a11f855b89
yhyuntak <yhyuntak@gmail.com>
2024-11-20 22:42:12 +0900

    [BUGFIX] eslint 에러 수정 하...

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,18 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  { files: ["**/*.{js,ts,jsx,tsx}"] },
-  {
-    languageOptions: {
-      globals: globals.browser, // window, console, alert 같은 browser 변수를 사용할 수 있도록 함.
-      ecmaVersion: "latest",
-      sourceType: "module",
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-    // rules: {
-    //   semi: ["error", "always"],
-    // },
+export default tseslint.config({
+  files: ['**/*.{ts,tsx}'],
+  extends: [pluginJs.configs.recommended, ...tseslint.configs.recommended],
+  languageOptions: {
+    globals: globals.browser, // window, console, alert 같은 browser 변수를 사용할 수 있도록 함.
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import eslintPlugin from "vite-plugin-eslint";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     eslintPlugin({
-      include: ["src/**/*.ts", "src/**/*.tsx", "src/**/*.js", "src/**/*.jsx"],
-      exclude: ["node_modules", "dist"],
+      include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.js', 'src/**/*.jsx'],
+      exclude: ['node_modules', 'dist'],
       failOnError: true, // 오류 발생 시 빌드 실패
       failOnWarning: false, // 경고는 무시
       cache: true, // ESLint 캐시 사용
@@ -17,22 +17,23 @@ export default defineConfig({
     }),
   ],
   server: {
+    hmr: true,
     port: 3000, // 개발 서버 포트 설정
     open: true,
     // localhost:3000/api/users 로 하는 요청을 http:/127.0.0.1:8080/users로 대체해서 서버로 요청을 보냄.
     proxy: {
-      "/api": {
-        target: "http://127.0.0.1:8080",
+      '/api': {
+        target: 'http://127.0.0.1:8080',
         changeOrigin: true,
         rewrite: (path: string) => {
-          return path.replace(/^\/api/, "");
+          return path.replace(/^\/api/, '');
         },
       },
     },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   // todo : build 관련 옵션을 나중에 수정해야함.
